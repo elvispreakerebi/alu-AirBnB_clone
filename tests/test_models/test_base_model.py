f1c9532323ffd4fd287b6eddd3d1c5f8fac71c8b
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2025-01-08 12:10:27 +0200

    added code for base model class and code for testing base model class

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module demonstrates the creation of an instance of the BaseModel class.
+"""
+from models.base_model import BaseModel
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key, value in my_model_json.items():
+    print(f"\t{key}: ({type(value).__name__}) - {value}")
