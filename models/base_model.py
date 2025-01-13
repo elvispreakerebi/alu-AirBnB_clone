d588de81592d45cbaf9b75cccdeca78990916a75
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2025-01-13 12:33:02 +0200

    removed id from init method of base model

--- models/base_model.py
+++ models/base_model.py
@@ -52,8 +52,6 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "id":
-                    value = str(value)
                 if key != "__class__":
                     setattr(self, key, value)
         else:
